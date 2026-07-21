3ba53a59e629034b7e6f0f0d395d25765082f3d3
ebinzzz <ebinbenny777@gmail.com>
2026-07-22 00:45:39 +0530

    fix(module-selector): include functions.php in select-module.php to resolve undefined function h() error

--- select-module.php
+++ select-module.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/includes/auth.php';
+require_once __DIR__ . '/includes/functions.php';
 
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
     header('Location: login.php');
